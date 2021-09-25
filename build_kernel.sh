44b874ff32dd1615b850b477824d1b04e6a47bae
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-09-25 18:55:58 +0300

    Add mt7601u driver

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,5 @@ export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
 
-make exynos7570-j4lte_defconfig
+make mt7601u_defconfig
 make -j$(nproc --all)
